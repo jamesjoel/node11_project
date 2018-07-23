779585c68fb8988c9e6aa787bd0a0a6d3c945aaf
jamesjoel <james.joelj@gmail.com>
2018-07-24 01:54:06 +0530

    searching

--- controller/home.js
+++ controller/home.js
@@ -3,16 +3,44 @@ var router = express.Router();
 var category = require("../model/category");
 var product = require("../model/product");
 router.get('/', function(req, res){
-	
-	product.find(function(err, result){
-		var productresult = result;
-		category.find(function(err, result){
-			var cateresult = result;
-			var pagedata = {title : "Home Page", pagename : "home/index", catedata : cateresult, prodata : productresult};
-			res.render("layout", pagedata);
+	if(req.query.search)
+	{
+		var q = req.query.search;
+
+		
+
+		var searchStr = (new RegExp("^"+q+""));
+		product.findWhere({ name : { $regex : searchStr }}, function(err, result){
+			var productresult = result;
+			category.find(function(err, result){
+				var cateresult = result;
+				var pagedata = {title : "Home Page", pagename : "home/index", catedata : cateresult, prodata : productresult};
+				res.render("layout", pagedata);
+			});
 		});
-	});
+
+	}
+	else
+	{
+
+		product.find(function(err, result){
+			var productresult = result;
+			category.find(function(err, result){
+				var cateresult = result;
+				var pagedata = {title : "Home Page", pagename : "home/index", catedata : cateresult, prodata : productresult};
+				res.render("layout", pagedata);
+			});
+		});
+
+	}
+
+	
 });
+
+
+
+
+
 
 // router.get("/:name/:id", function(req, res){
 // 	// console.log(req.params);
